dd3128e7ba1bcc362d1fefeb92f6a9df9f536e63
shourov.foisal <shourov.foisal@technonext.com>
2025-06-29 13:44:11 +0600

    output as json

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 import pandas as pd
 
-df = pd.read_csv("input/test.csv", encoding="utf8", nrows=5)
+df = pd.read_csv("input/test.csv", encoding="utf8", nrows=2)
+
 os.makedirs("output", exist_ok=True)
 df.to_csv("output/out.csv", index=False)
+
+os.makedirs("jsonout", exist_ok=True)
+df.to_json("jsonout/out.json", index=False, orient="records")
